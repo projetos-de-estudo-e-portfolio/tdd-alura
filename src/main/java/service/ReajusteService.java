7d350d280d4023dc928669b1413e3b01b4bbcc65
Luana da Rosa <lrosa.silva@outlook.com>
2021-12-14 13:04:23 -0300

    refatora service

--- src/main/java/service/ReajusteService.java
+++ src/main/java/service/ReajusteService.java
@@ -6,16 +6,10 @@ import br.com.alura.tdd.modelo.Funcionario;
 import java.math.BigDecimal;
 
 public class ReajusteService {
+
     public void concedeReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho == Desempenho.A_DESEJAR){
-           BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-           funcionario.reajustarSalario(reajuste);
-        } else if (desempenho == Desempenho.BOM){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.reajustarSalario(reajuste);
-        } else {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.reajustarSalario(reajuste);
-        }
+        BigDecimal percentual = desempenho.percentualReajuste();
+        BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+        funcionario.reajustarSalario(reajuste);
     }
 }
